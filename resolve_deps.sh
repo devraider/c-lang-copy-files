6d2b989be7db8bed7805e5b262d198b8ddf9aaec
crtpaul <cristeapaul.2015@gmail.com>
2025-05-28 22:42:19 +0300

    feat: dependency resolver

--- resolve_deps.sh
+++ resolve_deps.sh
@@ -10,5 +10,7 @@ mkdir -p ${UNITY_DIR}
 echo "Downloading Unity framework ⌛️"
 curl -s -o "$UNITY_DIR/unity.c" https://raw.githubusercontent.com/ThrowTheSwitch/Unity/master/src/unity.c
 curl -s -o "$UNITY_DIR/unity.h" https://raw.githubusercontent.com/ThrowTheSwitch/Unity/master/src/unity.h
+curl -s -o "$UNITY_DIR/unity_internals.h" https://raw.githubusercontent.com/ThrowTheSwitch/Unity/master/src/unity_internals.h
+curl -s -o "$UNITY_DIR/unity_config.h" https://raw.githubusercontent.com/ThrowTheSwitch/Unity/master/src/unity_config.h
 
 echo "✅Reolved dependencies🚀"
