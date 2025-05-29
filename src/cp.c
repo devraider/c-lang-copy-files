4574f24e3e612e2b645659e4e2531dd4428ce5d2
crtpaul <cristeapaul.2015@gmail.com>
2025-05-29 22:16:52 +0300

    fix: fixed source of files

--- src/cp.c
+++ src/cp.c
@@ -6,30 +6,39 @@ int cp(char *fileSrcPath, char *fileDstPath)
     FILE *srcFile, *dstFile;
 
     srcFile = fopen(fileSrcPath, "rb");
-    dstFile = fopen(fileDstPath, "rb+");
 
     // Check if the source file was opened successfully
-    if (srcFile == NULL || dstFile == NULL)
+    if (srcFile == NULL)
     {
-        // If either file could not be opened, print an error message
-        fprintf(stderr, "Error opening files: %s or %s\n", fileSrcPath, fileDstPath);
+        fprintf(stderr, "Error opening source file: %s\n", fileSrcPath);
+        fclose(srcFile);
+
         return 1;
     }
 
+    dstFile = fopen(fileDstPath, "wb");
+    if (dstFile == NULL)
+    {
+        // If either file could not be opened, print an error message
+        fprintf(stderr, "Error opening destination file: %s\n", fileDstPath);
+        fclose(srcFile);
+        fclose(dstFile);
+
+        return 1;
+    }
     char buffer[1024];
     size_t bytesRead;
+
     // Read from the source file and write to the destination file
     while ((bytesRead = fread(buffer, 1, sizeof(buffer), srcFile)) > 0)
     {
         fwrite(buffer, 1, bytesRead, dstFile);
     }
-    // Close both files
     fclose(srcFile);
     fclose(dstFile);
 
     printf("File copied successfully from %s to %s\n", fileSrcPath, fileDstPath);
     fflush(stdout);
 
-    // Return a success message
     return 0;
 }
